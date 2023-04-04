9f07a096944db77b615f56bc4a5fff5030413eb2
William <wifryo@gmail.com>
2023-04-04 18:03:58 +0200

    Complete next stage of bulls and cows

--- Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -4,10 +4,69 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    private static int getSecretLength() {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int secretCode = random.nextInt(10000);
+        return scanner.nextInt();
+    }
+
+    private static StringBuilder getRandomData() {
+        long pseudoRandomNumber = System.nanoTime();
+        StringBuilder bleb = new StringBuilder(String.valueOf(pseudoRandomNumber));
+        bleb.reverse();
+        return bleb;
+    }
+
+    private static String getSecret(int secretLength) {
+        StringBuilder secret = new StringBuilder(secretLength);
+        StringBuilder randomData = getRandomData();
+        int i = 0;
+
+        while (secret.length() < secretLength) {
+            Random rand = new Random();
+            // Add nonzero first number
+            if (secret.length() == 0) {
+                secret.append(rand.nextInt((9 - 1) + 1) + 1);
+
+            }
+            // If the end of randomData is reached, refresh it
+            if (i == randomData.length()) {
+                randomData = getRandomData();
+                i = 0;
+            }
+            // IndexOf returns -1 if no match is found, so the below means
+            // If the ith character of randomData is not already in secret
+            if (secret.toString().indexOf(randomData.charAt(i)) == -1 && secret.length() < secretLength) {
+                // Add it to secret
+                    secret.append(randomData.charAt(i));
+
+                }
+
+
+            i++;
+        }
+
+
+        return secret.toString();
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int secretLength = getSecretLength();
+        if (secretLength > 10) {
+            System.out.printf("Error: too number");
+        } else if (secretLength < 1){
+            System.out.println("Error: insufficient number");
+        }
+        else {
+            String secret = getSecret(secretLength);
+            System.out.printf("The random secret number is %s.", secret);
+        }
+    }
+
+        /*int secretCode = random.nextInt(10000);
         String secretCodeString = String.format("%04d", secretCode);
 
         int input = scanner.nextInt();
@@ -36,6 +95,8 @@ public class Main {
             System.out.println("Grade: " + bullsCount + " bull(s) and " + cowsCount + " cow(s). The secret code is " + secretCodeString + ".");
         } else {
             System.out.println("Grade: None. The secret code is " + secretCodeString + ".");
-        }
-    }
+        }*/
+
+
+
 }
